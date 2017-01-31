c1e8ca114e700d6a6e3e5bb10a4e3394d5ae7ffb
Pbra1484 <pbra1484@740s-pl-123.local>
2017-01-31 13:34:05 -0700

    made timer

--- FirstCPlusPlus/Model/Timer.cpp
+++ FirstCPlusPlus/Model/Timer.cpp
@@ -7,3 +7,37 @@
 //
 
 #include "Timer.hpp"
+#include <iostream>
+
+using namespace std;
+
+Timer :: Timer()
+{
+    executionTime = 0;
+}
+
+void Timer :: resetTimer()
+{
+    executionTime = 0;
+}
+
+void Timer :: startTimer()
+{
+    executionTime = clock();
+}
+
+void Timer :: finishTimer()
+{
+    executionTime = clock() - executionTime;
+}
+
+long Timer :: getExecutionTimeInMicroseconds()
+{
+    return executionTime;
+}
+
+void Timer :: displayTimerInformation()
+{
+    cout << "It took "<< executionTime << " to execute" << endl;
+    cout << "In seconds " << executionTime/CLOCKS_PER_SEC << endl;
+}
